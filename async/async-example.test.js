d832df686791aa8cda854667d5d00db1bbdd1ed9
AhmedEl-Mohankr <96636877+AhmedEl-Mohankr@users.noreply.github.com>
2024-04-13 17:04:06 +0100

    Update async-example.test.js

--- async/async-example.test.js
+++ async/async-example.test.js
@@ -4,10 +4,7 @@ import { generateToken, generateTokenPromise } from './async-example';
 
 it('should generate a token value', (done) => {
   const testUserEmail = 'test@test.com';
-
   generateToken(testUserEmail, (err, token) => {
-    // expect(token).toBeDefined();
-
     try {
       expect(token).toBeDefined();
       // expect(token).toBe(2);
